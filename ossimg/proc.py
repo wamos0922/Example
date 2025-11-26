7d51300fd51c5a63855019c9194e7558f0ef3b7b
wamos0922 <wamos1910@hanyang.ac.kr>
2025-11-26 10:54:23 +0900

    Base

---
+++ ossimg/proc.py
@@ -0,0 +1,45 @@
+
+
+
+# --- General Utility ---
+
+
+
+# --- Feature Implementations ---
+
+# 1. BRIGHTNESS (General Luminance Control)
+
+
+
+# 2. SATURATION (Color Intensity Control)
+
+
+
+
+# 3. SHARPNESS (Detail/Edge Control)
+
+
+
+
+# 4. SHADOWS (Tonal Control - Advanced Custom Curve)
+
+
+
+
+# --- TEMPLATE FUNCTIONS (Library Presets) ---
+
+#goldenhourtemplate
+
+#grittytemplate
+
+#pastelTemplate
+
+
+
+
+
+# --- MANUAL EDIT SEQUENCE (New Library Function) ---
+
+
+    
+# Do not forget to keep the setup.py file in the outer ossimg directory!
