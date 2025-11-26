8ec264a7687f3f4d9e47df8f17b76db8b49883fd
wamos0922 <wamos1910@hanyang.ac.kr>
2025-11-26 11:01:16 +0900

    close #1

--- ossimg/proc.py
+++ ossimg/proc.py
@@ -1,8 +1,12 @@
-
+from PIL import Image, ImageEnhance
+import math
+from typing import Generator, Tuple, Union
 
 
 # --- General Utility ---
-
+def load_image(path: str) -> Image.Image:
+    """Loads an image from a file path."""
+    return Image.open(path)
 
 
 # --- Feature Implementations ---
